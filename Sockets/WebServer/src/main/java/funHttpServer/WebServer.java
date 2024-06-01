a72c32a7c1b100770b03a90f22038abe16b1a51d
ecschul1 <schulte777@yahoo.com>
2024-06-01 01:03:31 -0700

    Error handling 3

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -198,12 +198,12 @@ class WebServer {
           // wrong data is given this just crashes
 
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-          // extract path parameters
-          query_pairs = splitQuery(request.replace("multiply?", ""));
 
           // extract required fields from parameters
           Integer num1, num2;
           try{
+          // extract path parameters
+          query_pairs = splitQuery(request.replace("multiply?", ""));
           num1 = Integer.parseInt(query_pairs.get("num1"));
           num2 = Integer.parseInt(query_pairs.get("num2"));
           }
